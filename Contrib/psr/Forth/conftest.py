77de9a6dd57b5e5b86de2a2b8ea8aa2e730eda4b
Peter Russell <qwertyface@gmail.com>
2019-11-21 22:57:46 +0000

    Tooling changes
    - Fix pytest command by adding an additional conftest file.
      This makes the tool add the root directory to the import path.
    - Switch build script to psake.
      Slightly better than my previous script.

---
+++ Contrib/psr/Forth/conftest.py
@@ -0,0 +1,1 @@
+# Here to convince py.test to include the root directory in sys.path
---
+++ Contrib/psr/Forth/psakefile.ps1
@@ -0,0 +1,53 @@
+$ErrorActionPreference = 'Stop';
+
+$dirExcludes = '.venv', '.pytest_cache'
+$fileExcludes = 'asm.py', 'dev.py', 'font_v3.py', 'gcl0x.py'
+$filesToFormat = get-childitem -Exclude $dirExcludes -Directory | % { get-childitem -path $_ -Recurse -Include '*.py' }
+$filesToFormat += get-childitem -Name '*.py' -Exclude $fileExcludes
+
+task default -depends Blacken, Test
+
+task Blacken {
+    & 'black' $filesToFormat
+    if ($LASTEXITCODE -ne 0 ) {
+        throw "Black failed";
+    }
+}
+
+task Test {
+    .\.venv\Scripts\pytest
+    if ($LASTEXITCODE -ne 0 ) {
+        throw "Test failure";
+    }
+}
+
+task Virtualenv {
+    try {
+        get-item '.venv' -ErrorAction Stop > $null
+    }
+    catch {
+        py -2 -m virtualenv '.\.venv'
+    }
+}
+
+task Packages -depends Virtualenv {
+    .\.venv\Scripts\pip install cffi ipython pytest hypothesis
+    if ($LASTEXITCODE -ne 0 ) {
+        throw "Packages failed"
+    }
+}
+
+task Extension -depends Packages {
+    .\.venv\Scripts\python.exe .\gtemu_extension_build.py
+    if ($LASTEXITCODE -ne 0 ) {
+        throw "Extension failed"
+    }
+}
+
+
+task ROM {
+    .\.venv\Scripts\python.exe .\dev.py Main=MainMenu\MainMenu.gcl Reset=Reset.gcl
+    if ($LASTEXITCODE -ne 0 ) {
+        throw "Rom failed";
+    }
+}

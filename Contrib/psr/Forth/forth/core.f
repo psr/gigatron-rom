afb5949c6fe8eb7790efdd481072eaef30cd893f
Peter Russell <qwertyface@gmail.com>
2020-02-17 14:24:33 +0000

    Implement ?DUP

--- Contrib/psr/Forth/forth/core.f
+++ Contrib/psr/Forth/forth/core.f
@@ -4,3 +4,5 @@
 : 0<> ( x -- flag )
     0= 0= ;
 
+: ?DUP ( x -- 0 | x x )
+    DUP IF DUP THEN ;
--- Contrib/psr/Forth/tests/test_core.py
+++ Contrib/psr/Forth/tests/test_core.py
@@ -2,7 +2,7 @@
 from hypothesis import given
 
 from asm import symbol
-from strategies import data_stack_depths, numbers
+from strategies import data_stack_depths, numbers, truth_values
 from utilities import do_test_word, get_IP, get_W, set_IP, set_W
 
 
@@ -40,4 +40,25 @@ def test_zero_not_equal(emulator, data_stack, return_stack, data_stack_depth, nu
 
     # Assert
     assert (number != 0) == data_stack.pop_flag()
+    assert data_stack_depth == len(data_stack)
+
+
+@given(data_stack_depth=data_stack_depths(with_room_for_values=2), number=truth_values)
+def test_question_dup(emulator, data_stack, return_stack, data_stack_depth, number):
+    # Arrange
+    data_stack.set_depth_in_bytes(data_stack_depth)
+    data_stack.push_word(number)
+    set_IP(0x4282)
+    set_W(symbol("forth.core.?DUP") + 4)
+    # Act
+    do_test_word(emulator, get_W())
+    while get_IP() != 0x4282:
+        do_test_word(emulator, "forth.next3.rom-mode")
+
+    # Assert
+    if number == 0:
+        assert 0 == data_stack.pop_i16()
+    else:
+        assert number == data_stack.pop_i16()
+        assert number == data_stack.pop_i16()
     assert data_stack_depth == len(data_stack)
